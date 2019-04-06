ad51b81b5685dbab0412dedbe60860df22f2b3d4
Smelky <46086711+Smelky@users.noreply.github.com>
2019-04-06 14:07:13 +0300

    Add

---
+++ src/main/java/CommentsApplication.java
@@ -0,0 +1,17 @@
+public class CommentsApplication {
+    public static void main(String[] args) throws Exception {
+        YoutubeCommentParser parser = new YoutubeCommentParser();
+        parser.init();
+        parser.getCommentsFromVideos("8n-qYptCU0A");
+        for (YoutubeVideo video : parser.getListOfVideos()) {
+            for (Comment comment : video.getComments()) {
+                System.out.println("Author: " + comment.getAuthor());
+                System.out.println("Comment: " +comment.getCommentText());
+                System.out.println("Upload date: " + comment.getUploadDate());
+                System.out.println("Likes: " + comment.getLikes());
+                System.out.println("Edited: " + comment.isEdited());
+                System.out.println("----------------------------------------");
+            }
+        }
+    }
+}
